074d44f2eaf7cd6388d24bce2d84032aaf7b9403
Philip Curley <curleypg@gmail.com>
2024-12-09 16:20:59 -0500

    submissions update

--- bingo/submissions.js
+++ bingo/submissions.js
@@ -1,1 +1,38 @@
-const submissions = []
+const submissions = [
+  {
+      "Timestamp": "12/9/2024 7:12:12",
+      "Discord username": "djwinch",
+      "Challenge completed": "During a precipitation",
+      "Additional information": "I can probably link cctv to show it was raining if required!",
+      "Activity date": "12/9/2024",
+      "Approved": "y",
+      "Public comment": ""
+  },
+  {
+      "Timestamp": "12/9/2024 8:10:48",
+      "Discord username": "lucasin16",
+      "Challenge completed": "At least 30min",
+      "Additional information": "N/A",
+      "Activity date": "12/9/2024",
+      "Approved": "y",
+      "Public comment": ""
+  },
+  {
+      "Timestamp": "12/9/2024 9:40:42",
+      "Discord username": "mricci",
+      "Challenge completed": "Sunrise run 🌄",
+      "Additional information": "7:03 AM\nTuesday, December 10, 2024 (EST)\nSunrise in Boston, MA",
+      "Activity date": "12/9/2024",
+      "Approved": "y",
+      "Public comment": ""
+  },
+  {
+      "Timestamp": "12/9/2024 10:58:44",
+      "Discord username": "jonahisokay",
+      "Challenge completed": "50m elevation gain ⛰️",
+      "Additional information": "",
+      "Activity date": "12/9/2024",
+      "Approved": "y",
+      "Public comment": ""
+  },
+]
