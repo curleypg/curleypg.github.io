61417c33da76bfb307815e1235377e1c070b0c80
Philip Curley <curleypg@gmail.com>
2024-12-10 09:18:25 -0500

    submissions

--- bingo/submissions.js
+++ bingo/submissions.js
@@ -1,38 +1,106 @@
 const submissions = [
   {
-      "Timestamp": "12/9/2024 7:12:12",
-      "Discord username": "djwinch",
-      "Challenge completed": "During a precipitation",
-      "Additional information": "I can probably link cctv to show it was raining if required!",
-      "Activity date": "12/9/2024",
-      "Approved": "y",
-      "Public comment": ""
+    "Timestamp": "12/9/2024 7:12:12",
+    "Discord username": "djwinch",
+    "Challenge completed": "During a precipitation",
+    "Activity date": "12/9/2024",
+    "Approved": "y",
+    "Public comment": ""
   },
   {
-      "Timestamp": "12/9/2024 8:10:48",
-      "Discord username": "lucasin16",
-      "Challenge completed": "At least 30min",
-      "Additional information": "N/A",
-      "Activity date": "12/9/2024",
-      "Approved": "y",
-      "Public comment": ""
+    "Timestamp": "12/9/2024 8:10:48",
+    "Discord username": "lucasin16",
+    "Challenge completed": "At least 30min",
+    "Activity date": "12/9/2024",
+    "Approved": "y",
+    "Public comment": ""
   },
   {
-      "Timestamp": "12/9/2024 9:40:42",
-      "Discord username": "mricci",
-      "Challenge completed": "Sunrise run 🌄",
-      "Additional information": "7:03 AM\nTuesday, December 10, 2024 (EST)\nSunrise in Boston, MA",
-      "Activity date": "12/9/2024",
-      "Approved": "y",
-      "Public comment": ""
+    "Timestamp": "12/9/2024 9:40:42",
+    "Discord username": "mricci",
+    "Challenge completed": "Sunrise run 🌄",
+    "Activity date": "12/9/2024",
+    "Approved": "y",
+    "Public comment": ""
   },
   {
-      "Timestamp": "12/9/2024 10:58:44",
-      "Discord username": "jonahisokay",
-      "Challenge completed": "50m elevation gain ⛰️",
-      "Additional information": "",
-      "Activity date": "12/9/2024",
-      "Approved": "y",
-      "Public comment": ""
+    "Timestamp": "12/9/2024 10:58:44",
+    "Discord username": "jonahisokay",
+    "Challenge completed": "50m elevation gain ⛰️",
+    "Activity date": "12/9/2024",
+    "Approved": "y",
+    "Public comment": ""
   },
+  {
+    "Timestamp": "12/9/2024 16:18:59",
+    "Discord username": "Striper",
+    "Challenge completed": "50m elevation gain ⛰️",
+    "Activity date": "12/9/2024",
+    "Approved": "y",
+    "Public comment": ""
+  },
+  {
+    "Timestamp": "12/9/2024 17:17:00",
+    "Discord username": "stuykidsarejedis",
+    "Challenge completed": "Run Commute 🏃‍♂️👜",
+    "Activity date": "12/9/2024",
+    "Approved": "y",
+    "Public comment": ""
+  },
+  {
+    "Timestamp": "12/9/2024 17:30:43",
+    "Discord username": "Unsmart",
+    "Challenge completed": "Night run: start between 9p-6a local time 🌌",
+    "Activity date": "12/9/2024",
+    "Approved": "y",
+    "Public comment": ""
+  },
+  {
+    "Timestamp": "12/9/2024 22:19:36",
+    "Discord username": "_h0si",
+    "Challenge completed": "Track run",
+    "Activity date": "12/9/2024",
+    "Approved": "y",
+    "Public comment": ""
+  },
+  {
+    "Timestamp": "12/9/2024 22:42:46",
+    "Discord username": "Shasta",
+    "Challenge completed": "At least 5km",
+    "Activity date": "12/9/2024",
+    "Approved": "y",
+    "Public comment": ""
+  },
+  {
+    "Timestamp": "12/9/2024 22:51:39",
+    "Discord username": "Jj",
+    "Challenge completed": "7th consecutive day",
+    "Activity date": "12/9/2024",
+    "Approved": "y",
+    "Public comment": ""
+  },
+  {
+    "Timestamp": "12/9/2024 23:45:52",
+    "Discord username": "Soup",
+    "Challenge completed": "At least 30min",
+    "Activity date": "12/9/2024",
+    "Approved": "y",
+    "Public comment": ""
+  },
+  {
+    "Timestamp": "12/10/2024 9:03:13",
+    "Discord username": "mricci",
+    "Challenge completed": "A single loop at least 10 times",
+    "Activity date": "12/10/2024",
+    "Approved": "y",
+    "Public comment": ""
+  },
+  {
+    "Timestamp": "12/10/2024 9:10:47",
+    "Discord username": "ossgeek",
+    "Challenge completed": "Tempo workout",
+    "Activity date": "12/10/2024",
+    "Approved": "y",
+    "Public comment": ""
+  }
 ]
