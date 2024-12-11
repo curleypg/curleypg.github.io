e180b342e92f9b40082d184e804dc48054ae9f95
Philip Curley <curleypg@gmail.com>
2024-12-11 09:19:36 -0500

    submissions

--- bingo/submissions.js
+++ bingo/submissions.js
@@ -76,8 +76,8 @@ const submissions = [
     "Discord username": "Jj",
     "Challenge completed": "7th consecutive day",
     "Activity date": "12/9/2024",
-    "Approved": "y",
-    "Public comment": ""
+    "Approved": "n",
+    "Public comment": "Selected wrong activity"
   },
   {
     "Timestamp": "12/9/2024 23:45:52",
@@ -98,6 +98,86 @@ const submissions = [
   {
     "Timestamp": "12/10/2024 9:10:47",
     "Discord username": "ossgeek",
+    "Challenge completed": "Tempo workout",
+    "Activity date": "12/10/2024",
+    "Approved": "y",
+    "Public comment": ""
+  },
+  {
+    "Timestamp": "12/10/2024 10:33:50",
+    "Discord username": "Jonahisokay",
+    "Challenge completed": "Strava Segment Top 3 Personal Effort 🏅",
+    "Activity date": "12/10/2024",
+    "Approved": "y",
+    "Public comment": ""
+  },
+  {
+    "Timestamp": "12/10/2024 10:36:56",
+    "Discord username": "lucasin16",
+    "Challenge completed": "Cross a body of water 🌊",
+    "Activity date": "12/10/2024",
+    "Approved": "y",
+    "Public comment": ""
+  },
+  {
+    "Timestamp": "12/10/2024 11:09:23",
+    "Discord username": "jj",
+    "Challenge completed": "Sunrise run 🌄",
+    "Activity date": "12/9/2024",
+    "Approved": "y",
+    "Public comment": ""
+  },
+  {
+    "Timestamp": "12/10/2024 12:21:53",
+    "Discord username": "Striper",
+    "Challenge completed": "Tempo workout",
+    "Activity date": "12/10/2024",
+    "Approved": "y",
+    "Public comment": ""
+  },
+  {
+    "Timestamp": "12/10/2024 17:29:47",
+    "Discord username": "Nomz",
+    "Challenge completed": "Double run day 🏃‍♂️🏃🏾‍♀️",
+    "Activity date": "12/10/2024",
+    "Approved": "y",
+    "Public comment": ""
+  },
+  {
+    "Timestamp": "12/10/2024 18:15:13",
+    "Discord username": "Striper",
+    "Challenge completed": "Cross-training activity",
+    "Activity date": "12/10/2024",
+    "Approved": "y",
+    "Public comment": ""
+  },
+  {
+    "Timestamp": "12/10/2024 18:29:33",
+    "Discord username": "Shasta",
+    "Challenge completed": "A single loop at least 10 times",
+    "Activity date": "12/10/2024",
+    "Approved": "y",
+    "Public comment": ""
+  },
+  {
+    "Timestamp": "12/10/2024 20:03:41",
+    "Discord username": "Unsmart",
+    "Challenge completed": "Out-and-back",
+    "Activity date": "12/10/2024",
+    "Approved": "y",
+    "Public comment": ""
+  },
+  {
+    "Timestamp": "12/10/2024 20:14:05",
+    "Discord username": "stuykidsarejedis",
+    "Challenge completed": "Run with someone 👫",
+    "Activity date": "12/10/2024",
+    "Approved": "y",
+    "Public comment": ""
+  },
+  {
+    "Timestamp": "12/10/2024 20:36:10",
+    "Discord username": "Soup",
     "Challenge completed": "Tempo workout",
     "Activity date": "12/10/2024",
     "Approved": "y",
